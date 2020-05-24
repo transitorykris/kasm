0e9947e056a6d59223a47811a6934467c08f9661
Kris Foster <kris.foster@gmail.com>
2020-05-24 11:32:55 -0700

    Remove debugging printlns

--- src/scanner.rs
+++ src/scanner.rs
@@ -13,9 +13,7 @@ pub fn scanner(raw_source: String) -> SourceTable {
 
     for raw_line in raw_source.split('\n') {
         let mut line = String::from(raw_line);
-        println!("raw line {}", line);
         line = line.trim().to_string();
-        println!("trimmed line {}", line);
 
         // We count all the lines to help the programmer with
         // finding errors later
@@ -28,16 +26,13 @@ pub fn scanner(raw_source: String) -> SourceTable {
             let (code, _) = line.split_at(comment_find.unwrap());
             line = String::from(code);
             line = line.trim().to_string(); // May have space between instruction and comment
-            println!("stripped comment {}", line);
         }
 
         if line.len() == 0 {
-            println!("Empty line, skipping");
             continue;
         }
 
         if line.starts_with(";") {
-            println!("Comment found, skipping");
             continue;
         }
         source.push(SourceLine { line, line_number });
