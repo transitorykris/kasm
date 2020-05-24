8639420eb006912c22984fcf6851d2de373d0e5d
Kris Foster <kris.foster@gmail.com>
2020-05-24 11:57:07 -0700

    Remove debugging printlns

--- src/assemble.rs
+++ src/assemble.rs
@@ -8,28 +8,22 @@ pub fn assemble(instruction_set: InstructionMap, program: Program, size: usize) 
     let mut count = 0;
     for line in program.code {
         count = count + 1;
-        println!("{}", count);
         let instruction_key = InstructionKey {
             mnemonic: line.0,
             address_mode: line.1,
         };
         let machine_code = instruction_set.get(&instruction_key);
-        print!("{:02x}", *machine_code.unwrap());
         output.push(*machine_code.unwrap());
         match line.2 {
             U8(val) => {
-                println!("{:02x}", val);
                 output.push(val);
             }
             U16(val) => {
-                println!("{:04x}", val);
                 let bytes = val.to_be_bytes();
                 output.push(bytes[1]); // Note: little endian!
                 output.push(bytes[0]);
             }
-            Null => {
-                println!("");
-            }
+            Null => {}
             String(label) => {
                 panic!("Found unresolved label: {}", label);
             }
